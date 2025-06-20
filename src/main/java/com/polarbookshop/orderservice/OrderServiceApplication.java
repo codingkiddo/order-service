d28358d71b775b7fbb48965292632cadb555df58
Vinod Kumar <vinodkumar@192.168.1.4>
2025-06-20 10:24:22 +0530

    modified:   src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java 	new file:   target/classes/com/polarbookshop/orderservice/OrderServiceApplication.class

--- src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
+++ src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
@@ -1,15 +1,22 @@
 package com.polarbookshop.orderservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
+	@Autowired
+	private Environment environment;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(OrderServiceApplication.class, args);
+		System.out.println(applicationContext.getBean(OrderServiceApplication.class).environment.getProperty("server.port"));
 	}
 
 }
